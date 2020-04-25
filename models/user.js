23a136c9f1d4f558c3e2b321d42e3ccf72e346b6
jake <jst4nt0n@gmail.com>
2020-04-25 18:05:00 -0400

    minor changes

--- models/user.js
+++ models/user.js
@@ -3,9 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    isAdmin: {
+
+    }
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
   };
   return User;
